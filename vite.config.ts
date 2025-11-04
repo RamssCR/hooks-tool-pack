b65aa859d24610470f57de31efd612d628661ee8
RamssCR <ramsesgpm@gmail.com>
2025-11-04 14:34:32 -0500

    ci: initial configuration

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig, coverageConfigDefaults } from 'vitest/config'
+import dts from 'vite-plugin-dts'
+import paths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), dts(), paths()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'hooks-tool-pack',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rolldownOptions: {
+      external: ['react'],
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'test/setupTest.ts',
+    include: ['test/**/*.test.{ts,tsx}'],
+    reporters: ['default', 'html'],
+    coverage: {
+      reporter: ['lcov', 'text'],
+      exclude: [
+        'node_modules',
+        '**/*.config.*',
+        'src/main.tsx',
+        'src/App.tsx',
+        'html/**',
+        'coverage/**',
+        'dist/**',
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
+  },
+})
