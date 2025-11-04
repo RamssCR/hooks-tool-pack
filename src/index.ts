34de9c63df0be4f4a0c7b599cc44401af228ff1f
RamssCR <ramsesgpm@gmail.com>
2025-11-04 16:28:18 -0500

    feat: created main entry file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { useIsMounted } from './hooks/useIsMounted'
+import { usePrevious } from './hooks/usePrevious'
+import { useToggle } from './hooks/useToggle'
+
+export { useToggle, useIsMounted, usePrevious }
