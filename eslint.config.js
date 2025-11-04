ac5b2327a761320fda435e1c52ecce2c6ce9c28c
RamssCR <ramsesgpm@gmail.com>
2025-11-04 14:46:50 -0500

    build: added React X plugin to the project's ESLint settings

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactX from 'eslint-plugin-react-x'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -13,6 +14,7 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
+      reactX.configs['strict-typescript'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
